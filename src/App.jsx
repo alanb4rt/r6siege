6908adf0c1e98ef8f4a5cd2f57639be1d0ca4ee4
BartAlan <al4n.b4rt@gmail.com>
2024-03-26 14:08:16 +0100

    add Rank component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
+import Rank from "./assets/components/Rank"
+
 export default function App() {
 
   return (
     <>
-    <header>
-      <h1>Rainbow Six : Siege<br/>Les opérateurs</h1>
-    </header>      
+      <header>
+        <h1>Rainbow Six : Siege<br/>Les opérateurs</h1>
+      </header>
+      <Rank />
     </>
   )
 }
